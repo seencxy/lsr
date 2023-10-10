817c85aae0ce8993a149c5e6c525e4b1ffbadb08
velit <2476358005@qq.com>
2023-10-10 19:03:21 +0800

    test_2

--- galxe/campaign_detail_all.go
+++ galxe/campaign_detail_all.go
@@ -4,7 +4,6 @@ import (
 	"bytes"
 	"encoding/json"
 	"io"
-	"log"
 	"net/http"
 )
 
@@ -21,181 +20,183 @@ type CampaignDetailAllRequest struct {
 
 // Campaign详情响应参数
 type CampaignDetailAllResponse struct {
-	Campaign struct {
-		CoHostSpaces []interface{} `json:"coHostSpaces"`
-		BannerUrl    string        `json:"bannerUrl"`
-		Id           string        `json:"id"`
-		Thumbnail    string        `json:"thumbnail"`
-		RewardName   string        `json:"rewardName"`
-		Type         string        `json:"type"`
-		Gamification struct {
-			Id          string        `json:"id"`
-			Type        string        `json:"type"`
-			Typename    string        `json:"__typename"`
-			ForgeConfig interface{}   `json:"forgeConfig"`
-			Nfts        []interface{} `json:"nfts"`
-			Airdrop     interface{}   `json:"airdrop"`
-		} `json:"gamification"`
-		Typename              string      `json:"__typename"`
-		NumberID              int         `json:"numberID"`
-		Chain                 string      `json:"chain"`
-		SpaceStation          interface{} `json:"spaceStation"`
-		Name                  string      `json:"name"`
-		Cap                   int         `json:"cap"`
-		Info                  string      `json:"info"`
-		UseCred               bool        `json:"useCred"`
-		Formula               string      `json:"formula"`
-		Status                string      `json:"status"`
-		Creator               string      `json:"creator"`
-		GasType               string      `json:"gasType"`
-		IsPrivate             bool        `json:"isPrivate"`
-		CreatedAt             string      `json:"createdAt"`
-		RequirementInfo       string      `json:"requirementInfo"`
-		Description           string      `json:"description"`
-		EnableWhitelist       bool        `json:"enableWhitelist"`
-		StartTime             int         `json:"startTime"`
-		EndTime               int         `json:"endTime"`
-		RequireEmail          bool        `json:"requireEmail"`
-		RequireUsername       bool        `json:"requireUsername"`
-		BlacklistCountryCodes string      `json:"blacklistCountryCodes"`
-		WhitelistRegions      string      `json:"whitelistRegions"`
-		RewardType            string      `json:"rewardType"`
-		DistributionType      string      `json:"distributionType"`
-		ClaimEndTime          interface{} `json:"claimEndTime"`
-		LoyaltyPoints         int         `json:"loyaltyPoints"`
-		TokenRewardContract   interface{} `json:"tokenRewardContract"`
-		TokenReward           struct {
-			UserTokenAmount      string `json:"userTokenAmount"`
-			TokenAddress         string `json:"tokenAddress"`
-			DepositedTokenAmount string `json:"depositedTokenAmount"`
-			TokenRewardId        int    `json:"tokenRewardId"`
-			TokenDecimal         string `json:"tokenDecimal"`
-			TokenLogo            string `json:"tokenLogo"`
-			TokenSymbol          string `json:"tokenSymbol"`
-			Typename             string `json:"__typename"`
-		} `json:"tokenReward"`
-		NftHolderSnapshot struct {
-			HolderSnapshotBlock int    `json:"holderSnapshotBlock"`
-			Typename            string `json:"__typename"`
-		} `json:"nftHolderSnapshot"`
-		WhitelistInfo struct {
-			Address                           string `json:"address"`
-			MaxCount                          int    `json:"maxCount"`
-			UsedCount                         int    `json:"usedCount"`
-			ClaimedLoyaltyPoints              int    `json:"claimedLoyaltyPoints"`
-			CurrentPeriodClaimedLoyaltyPoints int    `json:"currentPeriodClaimedLoyaltyPoints"`
-			CurrentPeriodMaxLoyaltyPoints     int    `json:"currentPeriodMaxLoyaltyPoints"`
-			Typename                          string `json:"__typename"`
-		} `json:"whitelistInfo"`
-		WhitelistSubgraph interface{}   `json:"whitelistSubgraph"`
-		Creds             []interface{} `json:"creds"`
-		CredentialGroups  []struct {
-			Id          string `json:"id"`
-			Description string `json:"description"`
-			Credentials []struct {
-				Id                    string      `json:"id"`
-				Name                  string      `json:"name"`
-				Type                  string      `json:"type"`
-				CredType              string      `json:"credType"`
-				CredSource            string      `json:"credSource"`
-				ReferenceLink         string      `json:"referenceLink"`
-				Description           string      `json:"description"`
-				LastUpdate            int         `json:"lastUpdate"`
-				SyncStatus            string      `json:"syncStatus"`
-				CredContractNFTHolder interface{} `json:"credContractNFTHolder"`
-				Chain                 string      `json:"chain"`
-				Eligible              int         `json:"eligible"`
-				Subgraph              interface{} `json:"subgraph"`
-				Metadata              struct {
-					VisitLink          interface{} `json:"visitLink"`
-					GitcoinPassport    interface{} `json:"gitcoinPassport"`
-					CampaignReferral   interface{} `json:"campaignReferral"`
-					GalxeScore         interface{} `json:"galxeScore"`
-					RestApi            interface{} `json:"restApi"`
-					WalletBalance      interface{} `json:"walletBalance"`
-					LensProfileFollow  interface{} `json:"lensProfileFollow"`
-					Graphql            interface{} `json:"graphql"`
-					LensPostUpvote     interface{} `json:"lensPostUpvote"`
-					LensPostMirror     interface{} `json:"lensPostMirror"`
-					MultiDimensionRest interface{} `json:"multiDimensionRest"`
-					Typename           string      `json:"__typename"`
-				} `json:"metadata"`
-				DimensionConfig string      `json:"dimensionConfig"`
-				Value           interface{} `json:"value"`
-				Typename        string      `json:"__typename"`
-			} `json:"credentials"`
-			ConditionRelation string `json:"conditionRelation"`
-			Conditions        []struct {
-				Expression      string   `json:"expression"`
-				Eligible        bool     `json:"eligible"`
-				EligibleAddress []string `json:"eligibleAddress"`
-				Typename        string   `json:"__typename"`
-			} `json:"conditions"`
-			Rewards []struct {
-				Expression  string `json:"expression"`
-				Eligible    bool   `json:"eligible"`
-				RewardCount int    `json:"rewardCount"`
-				RewardType  string `json:"rewardType"`
-				Typename    string `json:"__typename"`
-			} `json:"rewards"`
-			RewardAttrVals       interface{} `json:"rewardAttrVals"`
-			ClaimedLoyaltyPoints int         `json:"claimedLoyaltyPoints"`
-			Typename             string      `json:"__typename"`
-		} `json:"credentialGroups"`
-		Dao struct {
-			Id         string `json:"id"`
-			Name       string `json:"name"`
-			Logo       string `json:"logo"`
-			Alias      string `json:"alias"`
-			IsVerified bool   `json:"isVerified"`
-			Typename   string `json:"__typename"`
-			NftCores   struct {
+	Data struct {
+		Campaign struct {
+			CoHostSpaces []interface{} `json:"coHostSpaces"`
+			BannerUrl    string        `json:"bannerUrl"`
+			Id           string        `json:"id"`
+			Thumbnail    string        `json:"thumbnail"`
+			RewardName   string        `json:"rewardName"`
+			Type         string        `json:"type"`
+			Gamification struct {
+				Id          string        `json:"id"`
+				Type        string        `json:"type"`
+				Typename    string        `json:"__typename"`
+				ForgeConfig interface{}   `json:"forgeConfig"`
+				Nfts        []interface{} `json:"nfts"`
+				Airdrop     interface{}   `json:"airdrop"`
+			} `json:"gamification"`
+			Typename              string      `json:"__typename"`
+			NumberID              int         `json:"numberID"`
+			Chain                 string      `json:"chain"`
+			SpaceStation          interface{} `json:"spaceStation"`
+			Name                  string      `json:"name"`
+			Cap                   int         `json:"cap"`
+			Info                  string      `json:"info"`
+			UseCred               bool        `json:"useCred"`
+			Formula               string      `json:"formula"`
+			Status                string      `json:"status"`
+			Creator               string      `json:"creator"`
+			GasType               string      `json:"gasType"`
+			IsPrivate             bool        `json:"isPrivate"`
+			CreatedAt             string      `json:"createdAt"`
+			RequirementInfo       string      `json:"requirementInfo"`
+			Description           string      `json:"description"`
+			EnableWhitelist       bool        `json:"enableWhitelist"`
+			StartTime             int         `json:"startTime"`
+			EndTime               int         `json:"endTime"`
+			RequireEmail          bool        `json:"requireEmail"`
+			RequireUsername       bool        `json:"requireUsername"`
+			BlacklistCountryCodes string      `json:"blacklistCountryCodes"`
+			WhitelistRegions      string      `json:"whitelistRegions"`
+			RewardType            string      `json:"rewardType"`
+			DistributionType      string      `json:"distributionType"`
+			ClaimEndTime          interface{} `json:"claimEndTime"`
+			LoyaltyPoints         int         `json:"loyaltyPoints"`
+			TokenRewardContract   interface{} `json:"tokenRewardContract"`
+			TokenReward           struct {
+				UserTokenAmount      string `json:"userTokenAmount"`
+				TokenAddress         string `json:"tokenAddress"`
+				DepositedTokenAmount string `json:"depositedTokenAmount"`
+				TokenRewardId        int    `json:"tokenRewardId"`
+				TokenDecimal         string `json:"tokenDecimal"`
+				TokenLogo            string `json:"tokenLogo"`
+				TokenSymbol          string `json:"tokenSymbol"`
+				Typename             string `json:"__typename"`
+			} `json:"tokenReward"`
+			NftHolderSnapshot struct {
+				HolderSnapshotBlock int    `json:"holderSnapshotBlock"`
+				Typename            string `json:"__typename"`
+			} `json:"nftHolderSnapshot"`
+			WhitelistInfo struct {
+				Address                           string `json:"address"`
+				MaxCount                          int    `json:"maxCount"`
+				UsedCount                         int    `json:"usedCount"`
+				ClaimedLoyaltyPoints              int    `json:"claimedLoyaltyPoints"`
+				CurrentPeriodClaimedLoyaltyPoints int    `json:"currentPeriodClaimedLoyaltyPoints"`
+				CurrentPeriodMaxLoyaltyPoints     int    `json:"currentPeriodMaxLoyaltyPoints"`
+				Typename                          string `json:"__typename"`
+			} `json:"whitelistInfo"`
+			WhitelistSubgraph interface{}   `json:"whitelistSubgraph"`
+			Creds             []interface{} `json:"creds"`
+			CredentialGroups  []struct {
+				Id          string `json:"id"`
+				Description string `json:"description"`
+				Credentials []struct {
+					Id                    string      `json:"id"`
+					Name                  string      `json:"name"`
+					Type                  string      `json:"type"`
+					CredType              string      `json:"credType"`
+					CredSource            string      `json:"credSource"`
+					ReferenceLink         string      `json:"referenceLink"`
+					Description           string      `json:"description"`
+					LastUpdate            int         `json:"lastUpdate"`
+					SyncStatus            string      `json:"syncStatus"`
+					CredContractNFTHolder interface{} `json:"credContractNFTHolder"`
+					Chain                 string      `json:"chain"`
+					Eligible              int         `json:"eligible"`
+					Subgraph              interface{} `json:"subgraph"`
+					Metadata              struct {
+						VisitLink          interface{} `json:"visitLink"`
+						GitcoinPassport    interface{} `json:"gitcoinPassport"`
+						CampaignReferral   interface{} `json:"campaignReferral"`
+						GalxeScore         interface{} `json:"galxeScore"`
+						RestApi            interface{} `json:"restApi"`
+						WalletBalance      interface{} `json:"walletBalance"`
+						LensProfileFollow  interface{} `json:"lensProfileFollow"`
+						Graphql            interface{} `json:"graphql"`
+						LensPostUpvote     interface{} `json:"lensPostUpvote"`
+						LensPostMirror     interface{} `json:"lensPostMirror"`
+						MultiDimensionRest interface{} `json:"multiDimensionRest"`
+						Typename           string      `json:"__typename"`
+					} `json:"metadata"`
+					DimensionConfig string      `json:"dimensionConfig"`
+					Value           interface{} `json:"value"`
+					Typename        string      `json:"__typename"`
+				} `json:"credentials"`
+				ConditionRelation string `json:"conditionRelation"`
+				Conditions        []struct {
+					Expression      string   `json:"expression"`
+					Eligible        bool     `json:"eligible"`
+					EligibleAddress []string `json:"eligibleAddress"`
+					Typename        string   `json:"__typename"`
+				} `json:"conditions"`
+				Rewards []struct {
+					Expression  string `json:"expression"`
+					Eligible    bool   `json:"eligible"`
+					RewardCount int    `json:"rewardCount"`
+					RewardType  string `json:"rewardType"`
+					Typename    string `json:"__typename"`
+				} `json:"rewards"`
+				RewardAttrVals       interface{} `json:"rewardAttrVals"`
+				ClaimedLoyaltyPoints int         `json:"claimedLoyaltyPoints"`
+				Typename             string      `json:"__typename"`
+			} `json:"credentialGroups"`
+			Dao struct {
+				Id         string `json:"id"`
+				Name       string `json:"name"`
+				Logo       string `json:"logo"`
+				Alias      string `json:"alias"`
+				IsVerified bool   `json:"isVerified"`
+				Typename   string `json:"__typename"`
+				NftCores   struct {
+					List     []interface{} `json:"list"`
+					Typename string        `json:"__typename"`
+				} `json:"nftCores"`
+			} `json:"dao"`
+			RewardInfo struct {
+				DiscordRole             interface{} `json:"discordRole"`
+				Premint                 interface{} `json:"premint"`
+				LoyaltyPoints           interface{} `json:"loyaltyPoints"`
+				LoyaltyPointsMysteryBox interface{} `json:"loyaltyPointsMysteryBox"`
+				Typename                string      `json:"__typename"`
+			} `json:"rewardInfo"`
+			Participants struct {
+				ParticipantsCount  int    `json:"participantsCount"`
+				BountyWinnersCount int    `json:"bountyWinnersCount"`
+				Typename           string `json:"__typename"`
+			} `json:"participants"`
+			TaskConfig          interface{} `json:"taskConfig"`
+			ReferralCode        interface{} `json:"referralCode"`
+			RecurringType       string      `json:"recurringType"`
+			LatestRecurringTime interface{} `json:"latestRecurringTime"`
+			UserParticipants    struct {
 				List     []interface{} `json:"list"`
 				Typename string        `json:"__typename"`
-			} `json:"nftCores"`
-		} `json:"dao"`
-		RewardInfo struct {
-			DiscordRole             interface{} `json:"discordRole"`
-			Premint                 interface{} `json:"premint"`
-			LoyaltyPoints           interface{} `json:"loyaltyPoints"`
-			LoyaltyPointsMysteryBox interface{} `json:"loyaltyPointsMysteryBox"`
-			Typename                string      `json:"__typename"`
-		} `json:"rewardInfo"`
-		Participants struct {
-			ParticipantsCount  int    `json:"participantsCount"`
-			BountyWinnersCount int    `json:"bountyWinnersCount"`
-			Typename           string `json:"__typename"`
-		} `json:"participants"`
-		TaskConfig          interface{} `json:"taskConfig"`
-		ReferralCode        interface{} `json:"referralCode"`
-		RecurringType       string      `json:"recurringType"`
-		LatestRecurringTime interface{} `json:"latestRecurringTime"`
-		UserParticipants    struct {
-			List     []interface{} `json:"list"`
-			Typename string        `json:"__typename"`
-		} `json:"userParticipants"`
-		Space struct {
-			Id             string   `json:"id"`
-			Name           string   `json:"name"`
-			Info           string   `json:"info"`
-			Thumbnail      string   `json:"thumbnail"`
-			Alias          string   `json:"alias"`
-			Links          string   `json:"links"`
-			IsVerified     bool     `json:"isVerified"`
-			DiscordGuildID string   `json:"discordGuildID"`
-			FollowersCount int      `json:"followersCount"`
-			Typename       string   `json:"__typename"`
-			IsAdmin        bool     `json:"isAdmin"`
-			IsFollowing    bool     `json:"isFollowing"`
-			Categories     []string `json:"categories"`
-		} `json:"space"`
-		IsBookmarked         bool        `json:"isBookmarked"`
-		ClaimedLoyaltyPoints int         `json:"claimedLoyaltyPoints"`
-		ParentCampaign       interface{} `json:"parentCampaign"`
-		IsSequencial         bool        `json:"isSequencial"`
-		NumNFTMinted         int         `json:"numNFTMinted"`
-		ChildrenCampaigns    interface{} `json:"childrenCampaigns"`
-	} `json:"campaign"`
+			} `json:"userParticipants"`
+			Space struct {
+				Id             string   `json:"id"`
+				Name           string   `json:"name"`
+				Info           string   `json:"info"`
+				Thumbnail      string   `json:"thumbnail"`
+				Alias          string   `json:"alias"`
+				Links          string   `json:"links"`
+				IsVerified     bool     `json:"isVerified"`
+				DiscordGuildID string   `json:"discordGuildID"`
+				FollowersCount int      `json:"followersCount"`
+				Typename       string   `json:"__typename"`
+				IsAdmin        bool     `json:"isAdmin"`
+				IsFollowing    bool     `json:"isFollowing"`
+				Categories     []string `json:"categories"`
+			} `json:"space"`
+			IsBookmarked         bool        `json:"isBookmarked"`
+			ClaimedLoyaltyPoints int         `json:"claimedLoyaltyPoints"`
+			ParentCampaign       interface{} `json:"parentCampaign"`
+			IsSequencial         bool        `json:"isSequencial"`
+			NumNFTMinted         int         `json:"numNFTMinted"`
+			ChildrenCampaigns    interface{} `json:"childrenCampaigns"`
+		} `json:"campaign"`
+	} `json:"data"`
 }
 
 // 实现获取Campaign详情
@@ -246,8 +247,6 @@ func CampaignDetailAll(client http.Client, auth string, address string, id strin
 	if err != nil {
 		return *new(CampaignDetailAllResponse), err
 	}
-
-	log.Println(string(readAll))
 
 	var body CampaignDetailAllResponse
 
